a7a3f6d1bbaef98172add5141ddd8ff4016ce4a9
JOONHYUk_LEE <leejoon2067@gmail.com>
2024-08-12 20:25:57 +0900

    add: prompt_template 추가

--- app/core/config.py
+++ app/core/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
-    SERVER_NAME: str = "014B-AI-server"
+    SERVER_NAME: str = "014B-AI-Server"
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
@@ -19,7 +19,9 @@ class Settings(BaseSettings):
     # 추가된 필드
     LANGCHAIN_API_KEY: str
     OPENAI_API_KEY: str
-    AZURE_API_KEY: str
+
+    # AZURE_OPENAI_ENDPOINT: str
+    # AZURE_OPENAI_API_KEY: str
 
     class Config:
         env_file = ".env"
---
+++ app/question/prompts.py
@@ -0,0 +1,34 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from core.config import settings
+
+# 프롬프트 템플릿 초기화
+question_prompt_template = PromptTemplate.from_template(
+    """
+    You are responsible for answering questions to help farmers to facilitate in growth of their vegetables
+    Avoid non-essential questions.
+
+    You are a KOREAN urban farming expert assistant called '식집사'.
+    REMEMBER, your name is '식집사', so whatever user calls, you must perceive yourself as 식집사.
+    You are well-versed in crops and agricultural products that grow in urban environments. 
+    
+    If there are any links to Korean agricultural or related companies, feel free to provide those links to the user.     
+    And don't forget to give the answer in Korean and using the emoji often.
+
+    Output format:
+
+    answer : {answer}
+    """
+)
+
+translation_prompt_template = PromptTemplate.from_template(
+    """
+    Translate the given sentences into Korean in a natural way, following the format.
+    {answer}
+    """
+)
+
+prompt_templates: dict = {
+    "question": question_prompt_template,
+    "translation": translation_prompt_template,
+}
